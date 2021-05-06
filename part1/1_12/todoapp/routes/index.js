bd8fd0f3604c902c1fe9a170bb441e13b7fb0f15
Niklas Kuusisto <niklas.kuusisto95@gmail.com>
2021-05-06 19:36:07 +0300

    updated 1_12

--- part1/1_12/todoapp/routes/index.js
+++ part1/1_12/todoapp/routes/index.js
@@ -3,28 +3,33 @@ const axios = require('axios');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  let currentDate = new Date(Date.now()).toDateString();
-  fs.access(`./public/images/${currentDate}.jpg`, (err) => {
-    if (err) {
-      axios({
-        url: 'https://picsum.photos/480',
-        method: 'GET',
-        responseType: 'stream'
-    }).then(response => {
-      response.data.pipe(fs.createWriteStream(`./public/images/${currentDate}.jpg`));
-      res.render('index', { title: 'Express', imageUrl: `/images/${currentDate}.jpg` });
-      }).catch((err) => {
-        throw err;
-      })
-    } else {
-      res.render('index', { title: 'Express', imageUrl: `/images/${currentDate}.jpg` });
-    }
+const getImagePath = (currentDate) => {
+  return new Promise(resolve => {
+    fs.access(`./public/images/${currentDate}.jpg`, (err) => {
+      if (err) {
+        axios({
+          url: 'https://picsum.photos/480',
+          method: 'GET',
+          responseType: 'stream'
+      }).then(response => {
+        response.data.pipe(fs.createWriteStream(`./public/images/${currentDate}.jpg`));
+        resolve(`/images/${currentDate}.jpg`);
+        }).catch((err) => {
+          throw err;
+        })
+      } else {
+        resolve(`/images/${currentDate}.jpg`);
+      }
+    });
   });
+};
 
 
-
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const currentDate = new Date(Date.now()).toDateString();
+  const imagePath = await getImagePath(currentDate);
+  res.render('index', { title: 'Express', imageUrl: imagePath });
   
 });
 
