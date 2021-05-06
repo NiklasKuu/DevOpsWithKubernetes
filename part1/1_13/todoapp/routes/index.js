251ac231318e19d13c2fc8ff7dc22413718d4beb
Niklas Kuusisto <niklas.kuusisto95@gmail.com>
2021-05-06 20:03:38 +0300

    added exercise 1_13

---
+++ part1/1_13/todoapp/routes/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const axios = require('axios');
+var express = require('express');
+var router = express.Router();
+
+const getImagePath = (currentDate) => {
+  return new Promise(resolve => {
+    fs.access(`./public/images/${currentDate}.jpg`, (err) => {
+      if (err) {
+        axios({
+          url: 'https://picsum.photos/480',
+          method: 'GET',
+          responseType: 'stream'
+      }).then(response => {
+        response.data.pipe(fs.createWriteStream(`./public/images/${currentDate}.jpg`));
+        resolve(`/images/${currentDate}.jpg`);
+        }).catch((err) => {
+          throw err;
+        })
+      } else {
+        resolve(`/images/${currentDate}.jpg`);
+      }
+    });
+  });
+};
+
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const currentDate = new Date(Date.now()).toDateString();
+  const imagePath = await getImagePath(currentDate);
+
+  const todos = [
+  'todo1',
+  'todo2',
+  'todo3',
+  'todo4'
+  ];
+
+  res.render('index', { title: 'Express', imageUrl: imagePath, todos: todos });
+});
+
+module.exports = router;
